0f065675dc68374bcbdf490ba3574ebe04de7176
Jerry Velazquez <jvr572@gmail.com>
2019-05-07 17:49:03 -0400

    "login stuff"

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,24 @@
+class LoginsController < ApplicationController
+ 
+  def new
+   @errors = flash[:errors]
+  end
+
+  def create
+   @user = User.find_by(username: params[:username])
+   if @user && @user.authenticate(params[:password])
+     log_in_user(@user.id)
+     redirect_to anime_path
+   else
+     flash[:errors] = [ "User doesn't exist or invalid password" ]
+     redirect_to new_login_path
+   end
+  end
+
+  def destroy
+   log_out_user
+   redirect_to login_path
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_201745) do
+ActiveRecord::Schema.define(version: 2019_05_07_205820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2019_05_06_201745) do
     t.integer "season"
     t.string "summary"
     t.string "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
