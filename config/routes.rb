eb6389a1a148d9898dbd13da480b3625a40e7d94
Jerry Velazquez <jvr572@gmail.com>
2019-05-08 16:07:35 -0400

    "added home controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root'home#index'
   resources :logins
   resources :reviews
   resources :animes
-  resources :users
+
+
+resources :users
+resources :sessions, only: [:new, :create, :destroy]
+get ‘signup’, to: ‘users#new’, as: ‘signup’
+get ‘login’, to: ‘sessions#new’, as: ‘login’
+get ‘logout’, to: ‘sessions#destroy’, as: ‘logout’
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
