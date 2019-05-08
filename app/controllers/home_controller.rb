e4add458fb6b32b2d7bb2812596e0e4784ac204c
Jerry Velazquez <jvr572@gmail.com>
2019-05-08 16:59:44 -0400

    "added index"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @anime = Anime.all
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,10 @@ Rails.application.routes.draw do
   resources :logins
   resources :reviews
   resources :animes
+  resources :home, only: [:index, :show ]
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-
-resources :users
-resources :sessions, only: [:new, :create, :destroy]
-get ‘signup’, to: ‘users#new’, as: ‘signup’
-get ‘login’, to: ‘sessions#new’, as: ‘login’
-get ‘logout’, to: ‘sessions#destroy’, as: ‘logout’
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
